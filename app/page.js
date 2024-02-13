831ed62f647f32499fe701c3e374679279897f57
Kanisevsky <meltsmokestudio@gmail.com>
2024-02-13 21:51:45 +0000

    ContactCard component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Alert from '@/components/Alert';
+import ContactCard from '@/components/ContactCard';
 
 export default function Home() {
   return (
     <main>
       <Alert />
+      <ContactCard />
     </main>
   );
 }
